ceced4c8906cf8e4820e4cfce58f2ca0f4202781
Miles Gray <mgray16@gmail.com>
2020-12-29 08:04:47 -0600

    Additional coord functions

--- utils.py
+++ utils.py
@@ -96,6 +96,63 @@ def resize_fn(img, size):
         transforms.Resize(size, Image.BICUBIC)(
             transforms.ToPILImage()(img)))
 
+
+def generate_coords(batch_size: int, img_size: int) -> Tensor:
+    row = torch.arange(0, img_size).float() / img_size # [img_size]
+    x_coords = row.view(1, -1).repeat(img_size, 1) # [img_size, img_size]
+    y_coords = x_coords.t() # [img_size, img_size]
+
+    coords = torch.stack([x_coords, y_coords], dim=2) # [img_size, img_size, 2]
+    coords = coords.view(-1, 2) # [img_size ** 2, 2]
+    coords = coords.t().view(1, 2, img_size ** 2).repeat(batch_size, 1, 1) # [batch_size, 2, n_coords]
+
+    return coords
+
+def generate_var_sized_coords(aspect_ratios: List[float], img_size: int) -> Tensor:
+    """
+    Generates variable-sized coordinates for images with padding.
+    This is actually done by generating "normal" coordinates, but using
+    a range beyond [0, 1] for a shorter side.
+    Aspect ratio is assumed to be equal to w/h.
+    The goal of this functino is two constrain the spacing
+    """
+    coords = generate_coords(len(aspect_ratios), img_size) # [batch_size, 2, img_size ** 2]
+    scales = [([1.0 / ar, 1.0] if ar < 1.0 else [1.0, ar]) for ar in aspect_ratios] # [batch_size, 2]
+    scales = torch.tensor(scales).unsqueeze(2) # [batch_size, 2, 1]
+    coords_scaled = coords * scales
+
+    return coords_scaled
+
+def generate_random_resolution_coords(batch_size: int, img_size: int, scale: float=None, min_scale: float=None) -> Tensor:
+    """
+    Generating random input coordinate patches.
+    It's used when we train on random image patches of different resolution
+    """
+    assert (int(scale is None) + int(min_scale is None)) == 1, "Either scale or min_scale should be specified."
+
+    if scale is None:
+        sizes = np.random.rand(batch_size) * (1 - min_scale) + min_scale # Changing the range: [0, 1] => [min_scale, 1]
+        scale = min_scale
+    else:
+        sizes = np.ones(batch_size) * scale # [batch_size]
+
+    x_offsets = np.random.rand(batch_size) * (1 - scale) # [batch_size]
+    y_offsets = np.random.rand(batch_size) * (1 - scale) # [batch_size]
+
+    # Unfortunately, torch.linspace cannot operate on a batch of inputs
+    x_coords = torch.from_numpy(np.linspace(x_offsets, x_offsets + sizes, img_size, dtype=np.float32)) # [img_size, batch_size]
+    y_coords = torch.from_numpy(np.linspace(y_offsets, y_offsets + sizes, img_size, dtype=np.float32)) # [img_size, batch_size]
+
+    x_coords = x_coords.view(1, img_size, batch_size).repeat(1, img_size, 1) # [img_size, img_size, batch_size]
+    y_coords = y_coords.view(img_size, 1, batch_size).repeat(1, img_size, 1) # [img_size, img_size, batch_size]
+
+    x_coords = x_coords.view(img_size ** 2, batch_size) # [img_size ** 2, batch_size]
+    y_coords = y_coords.view(img_size ** 2, batch_size) # [img_size ** 2, batch_size]
+
+    coords = torch.stack([x_coords, y_coords], dim=0).permute(2, 0, 1) # [batch_size, 2, img_size ** 2]
+
+    return coords
+
 def make_coord(shape, ranges=None, flatten=True):
     """ Make coordinates at grid centers.
     """
