98b3613f2a518f3c2d009cf7f312b6a395d1687e
Miles Gray <mgray16@gmail.com>
2020-12-31 13:13:14 -0600

    Add liif_inr to model init

--- models/__init__.py
+++ models/__init__.py
@@ -2,5 +2,5 @@ from .models import register, make
 from . import edsr, rdn, rcan, ddbpn
 from . import freq_discriminator, mlp_discriminator
 from . import mlp
-from . import liif, liif_inr
+from . import liif, inr_liif
 from . import misc
