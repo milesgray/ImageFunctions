e731ca75d62e144c5c98ba8b945ba9fd5c061907
Miles Gray <mgray16@gmail.com>
2021-04-05 21:02:22 -0500

    Add DexiNED edge detection models

---
+++ models/dexined_v3.py
@@ -0,0 +1,379 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from kornia.geometry.subpix import spatial_softmax2d
+
+
+###################################################
+#### START PIXEL ATTENTION ##########################
+
+def default_conv(in_channels, out_channels, kernel_size, bias=True):
+    return nn.Conv2d(
+        in_channels, out_channels, kernel_size,
+        padding=(kernel_size//2), bias=bias)
+
+
+class SpatialSoftmax2d(nn.Module):
+    def __init__(self, temp=1.0):
+        super().__init__()
+        self.temp = temp
+
+    def forward(self, x):
+        x = spatial_softmax2d(x, temperature=self.temp)
+        return x
+
+class Scale(nn.Module):
+    def __init__(self, init_value=1e-3):
+        super().__init__()
+        self.scale = nn.Parameter(torch.FloatTensor([init_value]))
+
+    def forward(self, input):
+        return input * self.scale
+
+class PA(nn.Module):
+    '''Pixel Attention Layer'''
+    def __init__(self, f_in, f_out=None, resize="same", scale=2, softmax=True, learn_weight=True, channel_wise=True, spatial_wise=True):
+        super().__init__()
+        if f_out is None:
+            f_out = f_in
+        
+        self.sigmoid = nn.Sigmoid()
+        if resize == "up":
+            self.resize = nn.Upsample(scale_factor=scale, mode="bilinear", align_corners=True)
+        elif resize == "down":
+            self.resize = nn.AvgPool2d(scale, stride=scale)
+        else:
+            self.resize = nn.Identity()
+        if f_in != f_out:
+            self.resize = nn.Sequential(*[self.resize, nn.Conv2d(f_in, f_out, 1)])
+        self.channel_wise = channel_wise
+        self.spatial_wise = spatial_wise
+        if channel_wise:
+            self.channel_conv = nn.Conv2d(f_out, f_out, 1, groups=f_out)
+        if spatial_wise:
+            self.spatial_conv = nn.Conv2d(f_out, f_out, 1)
+        if not channel_wise and not spatial_wise:
+            self.conv = nn.Conv2d(f_out, f_out, 1)
+        
+        self.use_softmax = softmax
+        if self.use_softmax:
+            self.spatial_softmax = SpatialSoftmax2d()
+            self.channel_softmax = nn.Softmax2d()
+        self.learn_weight = learn_weight
+        if self.learn_weight:
+            self.weight_scale = Scale(1.0)
+
+    def forward(self, x):
+        x = self.resize(x)
+        if self.spatial_wise:
+            spatial_y = self.spatial_conv(x)
+            spatial_y = self.sigmoid(spatial_y)
+            if self.use_softmax:
+                spatial_y = self.spatial_softmax(spatial_y)
+            spatial_out = torch.mul(x, spatial_y)
+        if self.channel_wise:
+            channel_y = self.channel_conv(x)
+            channel_y = self.sigmoid(channel_y)
+            if self.use_softmax:
+                channel_y = self.channel_softmax(channel_y)
+            channel_out = torch.mul(x, channel_y)
+        if self.channel_wise and self.spatial_wise:
+            out = spatial_out + channel_out
+        elif self.channel_wise:
+            out = channel_wise
+        elif self.spatial_wise:
+            out = spatial_wise
+        else:
+            y = self.conv(x)
+            y = self.sigmoid(y)
+            if self.use_softmax:
+                y = self.spatial_softmax(y)
+            out = torch.mul(x, y)
+        if self.learn_weight:
+            out = self.weight_scale(out)
+        return out
+
+#### END PIXEL ATTENTION ##########################
+###################################################
+
+def weight_init(m):
+    if isinstance(m, (nn.Conv2d,)):
+        # print("Applying custom weight initialization for nn.Conv2d layer...")
+        # torch.nn.init.kaiming_uniform_(m.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        torch.nn.init.xavier_normal_(m.weight, gain=1.0)
+        # torch.nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('relu'))
+        # torch.nn.init.normal_(m.weight, mean=0, std=0.01)
+        if m.weight.data.shape[1] == torch.Size([1]):
+            torch.nn.init.normal_(m.weight, mean=0.0,)
+        if m.weight.data.shape == torch.Size([1, 6, 1, 1]):
+            torch.nn.init.constant_(m.weight, 0.2) # for fuse conv
+        if m.bias is not None:
+            torch.nn.init.zeros_(m.bias)
+
+    # for fusion layer
+    if isinstance(m, (nn.ConvTranspose2d,)):
+        torch.nn.init.xavier_normal_(m.weight, gain=1.0)
+        # torch.nn.init.kaiming_uniform_(m.weight, a=0, mode='fan_in', nonlinearity='leaky_relu')
+        # torch.nn.init.xavier_uniform_(m.weight, gain=nn.init.calculate_gain('relu'))
+        # torch.nn.init.normal_(m.weight, mean=0, std=0.01)
+        if m.weight.data.shape[1] == torch.Size([1]):
+            torch.nn.init.normal_(m.weight, std=0.1)
+        if m.bias is not None:
+            torch.nn.init.zeros_(m.bias)
+
+
+class _DenseLayer(nn.Sequential):
+    def __init__(self, input_features, out_features):
+        super(_DenseLayer, self).__init__()
+
+        # self.add_module('relu2', nn.ReLU(inplace=True)),
+        self.add_module('conv1', nn.Conv2d(input_features, out_features,
+                                           kernel_size=3, stride=1, padding=2, bias=True)),
+        self.add_module('norm1', nn.BatchNorm2d(out_features)),
+        self.add_module('relu1', nn.ReLU(inplace=True)),
+        self.add_module('conv2', nn.Conv2d(out_features, out_features,
+                                           kernel_size=3, stride=1, bias=True)),
+        self.add_module('norm2', nn.BatchNorm2d(out_features))
+        # double check the norm1 comment if necessary and put norm after conv2
+
+    def forward(self, x):
+        x1, x2 = x
+
+        new_features = super(_DenseLayer, self).forward(F.relu(x1))  # F.relu()
+        # if new_features.shape[-1]!=x2.shape[-1]:
+        #     new_features =F.interpolate(new_features,size=(x2.shape[2],x2.shape[-1]), mode='bicubic',
+        #                                 align_corners=False)
+        return 0.5 * (new_features + x2), x2
+
+
+class _DenseBlock(nn.Sequential):
+    def __init__(self, num_layers, input_features, out_features):
+        super(_DenseBlock, self).__init__()
+        for i in range(num_layers):
+            layer = _DenseLayer(input_features, out_features)
+            self.add_module('denselayer%d' % (i + 1), layer)
+            input_features = out_features
+
+
+class UpConvBlock(nn.Module):
+    def __init__(self, in_features, up_scale, up_factor = 2):
+        super(UpConvBlock, self).__init__()
+        self.up_factor = up_factor
+        self.constant_features = 16
+        layers = self.make_deconv_layers(in_features, up_scale)
+        assert layers is not None, layers
+        self.features = nn.Sequential(*layers)
+
+    def make_deconv_layers(self, in_features, up_scale):
+        layers = []
+        all_pads=[0,0,1,3,7]
+        for i in range(up_scale):
+            kernel_size = self.up_factor ** up_scale
+            pad = all_pads[up_scale]  # kernel_size-1
+            out_features = self.compute_out_features(i, up_scale)
+            bottleneck_features = int(self.constant_features * max(1,i))
+            layers.append(nn.Conv2d(in_features, bottleneck_features, 1))
+            layers.append(nn.BatchNorm2d(bottleneck_features))
+            layers.append(nn.ReLU(inplace=True))
+            layers.append(nn.Conv2d(bottleneck_features, int(out_features * (self.up_factor ** 2)), 1))
+            layers.append(nn.Hardtanh(-5,5))
+            
+            if i % 2 == 0 or up_scale == i and out_features > 4:
+                layers.append(nn.PixelShuffle(self.up_factor))
+                layers.append(nn.Conv2d(out_features, out_features, 1))
+            else:
+                layers.append(PA(int(out_features * (self.up_factor ** 2)), out_features, resize="up"))
+                layers.append(nn.Conv2d(out_features, out_features, 3, stride=2, padding=1))
+                layers.append(nn.ConvTranspose2d(
+                    out_features, out_features, kernel_size, stride=self.up_factor, padding=pad))            
+                layers.append(nn.Conv2d(out_features, out_features, 1))
+            
+            in_features = out_features
+        return layers
+
+    def compute_out_features(self, idx, up_scale):
+        return 1 if idx == up_scale - 1 else self.constant_features
+
+    def forward(self, x):
+        return self.features(x)
+
+
+class SingleConvBlock(nn.Module):
+    def __init__(self, in_features, out_features, stride,
+                 use_bs=True
+                 ):
+        super(SingleConvBlock, self).__init__()
+        self.use_bn = use_bs
+        self.conv = nn.Conv2d(in_features, out_features, 1, stride=stride,
+                              bias=True)
+        self.bn = nn.BatchNorm2d(out_features)
+
+    def forward(self, x):
+        x = self.conv(x)
+        if self.use_bn:
+            x = self.bn(x)
+        return x
+
+
+class DoubleConvBlock(nn.Module):
+    def __init__(self, in_features, mid_features,
+                 out_features=None,
+                 stride=1,
+                 use_act=True):
+        super(DoubleConvBlock, self).__init__()
+
+        self.use_act = use_act
+        if out_features is None:
+            out_features = mid_features
+        self.conv1 = nn.Conv2d(in_features, mid_features,
+                               3, padding=1, stride=stride)
+        self.bn1 = nn.BatchNorm2d(mid_features)
+        self.conv2 = nn.Conv2d(mid_features, out_features, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_features)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        if self.use_act:
+            x = self.relu(x)
+        return x
+
+
+class DexiNed(nn.Module):
+    """ Definition of the DXtrem network. """
+
+    def __init__(self):
+        super(DexiNed, self).__init__()
+        self.block_1 = DoubleConvBlock(3, 32, 64, stride=2,)
+        self.block_2 = DoubleConvBlock(64, 128, use_act=False)
+        self.dblock_3 = _DenseBlock(2, 128, 256)
+        self.dblock_4 = _DenseBlock(3, 256, 512)
+        self.dblock_5 = _DenseBlock(3, 512, 512)
+        self.dblock_6 = _DenseBlock(3, 512, 256)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        # left skip connections, figure in Journal
+        self.side_1 = SingleConvBlock(64, 128, 2)
+        self.side_2 = SingleConvBlock(128, 256, 2)
+        self.side_3 = SingleConvBlock(256, 512, 2)
+        self.side_4 = SingleConvBlock(512, 512, 1)
+        self.side_5 = SingleConvBlock(512, 256, 1)
+
+        # right skip connections, figure in Journal
+        self.pre_dense_2 = SingleConvBlock(128, 256, 2, use_bs=False)
+        self.pre_dense_3 = SingleConvBlock(128, 256, 1)
+        self.pre_dense_4 = SingleConvBlock(256, 512, 1)
+        self.pre_dense_5_0 = SingleConvBlock(256, 512, 2,use_bs=False)
+        self.pre_dense_5 = SingleConvBlock(512, 512, 1)
+        self.pre_dense_6 = SingleConvBlock(512, 256, 1)
+
+        # USNet
+        self.up_block_1 = UpConvBlock(64, 1)
+        self.up_block_2 = UpConvBlock(128, 1)
+        self.up_block_3 = UpConvBlock(256, 2)
+        self.up_block_4 = UpConvBlock(512, 3)
+        self.up_block_5 = UpConvBlock(512, 4)
+        self.up_block_6 = UpConvBlock(256, 4)
+        self.block_cat = SingleConvBlock(6, 1, stride=1, use_bs=False)
+
+        self.apply(weight_init)
+
+    def slice(self, tensor, slice_shape):
+        t_shape = tensor.shape
+        height, width = slice_shape
+        if t_shape[-1]!=slice_shape[-1]:
+            new_tensor = F.interpolate(
+                tensor, size=(height, width), mode='bicubic',align_corners=False)
+        else:
+            new_tensor=tensor
+        # tensor[..., :height, :width]
+        return new_tensor
+
+    def forward(self, x):
+        assert x.ndim == 4, x.shape
+
+        # Block 1
+        # print(f"x shape           : {x.shape}")
+        block_1 = self.block_1(x)
+        # print(f"block_1 shape     : {block_1.shape}")
+        block_1_side = self.side_1(block_1)
+        # print(f"block_1_side shape: {block_1_side.shape}")
+
+        # Block 2
+        block_2 = self.block_2(block_1)
+        block_2_down = self.maxpool(block_2)
+        block_2_add = block_2_down + block_1_side
+        block_2_side = self.side_2(block_2_add)
+
+        # Block 3
+        block_3_pre_dense = self.pre_dense_3(block_2_down)
+        block_3, _ = self.dblock_3([block_2_add, block_3_pre_dense])
+        block_3_down = self.maxpool(block_3)
+        block_3_add = block_3_down + block_2_side
+        block_3_side = self.side_3(block_3_add)
+
+        # Block 4
+        block_4_pre_dense_256 = self.pre_dense_2(block_2_down)
+        block_4_pre_dense = self.pre_dense_4(
+            block_4_pre_dense_256 + block_3_down)
+        block_4, _ = self.dblock_4([block_3_add, block_4_pre_dense])
+        block_4_down = self.maxpool(block_4)
+        block_4_add = block_4_down + block_3_side
+        block_4_side = self.side_4(block_4_add)
+
+        # Block 5
+        block_5_pre_dense_512 = self.pre_dense_5_0(block_4_pre_dense_256)
+        block_5_pre_dense = self.pre_dense_5(
+            block_5_pre_dense_512 + block_4_down)
+        block_5, _ = self.dblock_5([block_4_add, block_5_pre_dense])
+        block_5_add = block_5 + block_4_side
+
+        # Block 6
+        block_6_pre_dense = self.pre_dense_6(block_5)
+        block_6, _ = self.dblock_6([block_5_add, block_6_pre_dense])
+
+        # upsampling blocks
+        # height, width = x.shape[-2:]
+        # slice_shape = (height, width)
+        # out_1 = self.slice(self.up_block_1(block_1), slice_shape)
+        out_1 = self.up_block_1(block_1)
+        out_2 = self.up_block_2(block_2)
+        out_3 = self.up_block_3(block_3)
+        out_4 = self.up_block_4(block_4)
+        out_5 = self.up_block_5(block_5)
+        out_6 = self.up_block_6(block_6)
+        results = [out_1, out_2, out_3, out_4, out_5, out_6]
+        # print(out_1.shape)
+
+        # concatenate multiscale outputs
+        block_cat = torch.cat(results, dim=1)  # Bx6xHxW
+        block_cat = self.block_cat(block_cat)  # Bx1xHxW
+
+        # return results
+        results.append(block_cat)
+        return results
+
+
+if __name__ == '__main__':
+    batch_size = 8
+    img_height = 400
+    img_width = 400
+
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    device = "cpu"
+    input = torch.rand(batch_size, 3, img_height, img_width).to(device)
+    # target = torch.rand(batch_size, 1, img_height, img_width).to(device)
+    print(f"input shape: {input.shape}")
+    model = DexiNed().to(device)
+    output = model(input)
+    print(f"output shapes: {[t.shape for t in output]}")
+
+    # for i in range(20000):
+    #     print(i)
+    #     output = model(input)
+    #     loss = nn.MSELoss()(output[-1], target)
+    #     loss.backward()
