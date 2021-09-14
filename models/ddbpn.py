fix bug with update to korniapatial softmax 2d

models/ddbpn.py
@@ -310,9 +310,9 @@ class MeanShift(nn.Conv2d):
             p.requires_grad = False
 
 class SpatialSoftmax2d(nn.Module):
-    def __init__(self, temp=1.0):
+    def __init__(self, temp=1.0, learn_temp=True):
         super().__init__()
-        self.temp = temp
+        self.temp = nn.Parameter(temp, requires_grad=learn_temp)
 
     def forward(self, x):
         x = spatial_softmax2d(x, temperature=self.temp)

models/liif.py
@@ -67,7 +67,10 @@ class LIIF(nn.Module):
         if torch.cuda.is_available():
             feat_coord = feat_coord.cuda()
         feat_coord = feat_coord.permute(2, 0, 1) \
-                      .unsqueeze(0).expand(feat.shape[0], self.encoder.out_dim, *feat.shape[-2:])
+                      .unsqueeze(0) \
+                          .expand(feat.shape[0], 
+                                  self.encoder.out_dim, 
+                                  *feat.shape[-2:])
 
         preds = []
         areas = []