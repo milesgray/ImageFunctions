5af46e6a2b6b134930732c74622c69dd9e0e6b0f
Miles Gray <mgray16@gmail.com>
2021-11-16 21:33:33 -0600

    Fix dexined_v3 import

--- models/__init__.py
+++ models/__init__.py
@@ -4,4 +4,4 @@ from . import freq_discriminator, mlp_discriminator
 from . import mlp
 from . import liif, inr_liif
 from . import misc
-from . import dexined_v2, dexined_v2
+from . import dexined_v2, dexined_v3
