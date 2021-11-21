Fix issues with kornia changes to 2d softmax, add trainig config for LIIF on 'feedback' datasets, add new attention mechanism 'local multi-head channel self attention'

models/inr_liif.py
@@ -74,7 +74,6 @@ class FourierINR(nn.Module):
         return self.model(self.compute_fourier_feats(coords))
 
 
-@register('liif_inr')
 class LIIF_INR(nn.Module):
     def __init__(self, encoder_spec, imnet_spec=None,
                  local_ensemble=True, feat_unfold=True, cell_decode=True,
@@ -191,3 +190,10 @@ class LIIF_INR(nn.Module):
         self.gen_feat(inp)
         return self.query_rgb(coord, cell)
 
+
+@register('liif_inr')
+def make_liif_inr(local_ensemble=True, feat_unfold=True, cell_decode=True,
+                  fourier_features=64):
+    args = Namespace()
+    args.local_ensemble=local_ensemble
+    args.feat_unfold

models/rdn.py
@@ -13,7 +13,7 @@ from models import register
 class SpatialSoftmax2d(nn.Module):
     def __init__(self, temp=1.0):
         super().__init__()
-        self.temp = temp
+        self.temp = nn.Parameter(torch.FloatTensor([temp]), requires_grad=True)
 
     def forward(self, x):
         x = spatial_softmax2d(x, temperature=self.temp)
@@ -22,7 +22,7 @@ class SpatialSoftmax2d(nn.Module):
 class Scale(nn.Module):
     def __init__(self, init_value=1e-3):
         super().__init__()
-        self.scale = nn.Parameter(torch.FloatTensor([init_value]))
+        self.scale = nn.Parameter(torch.FloatTensor([init_value]), requires_grad=True)
 
     def forward(self, input):
         return input * self.scale