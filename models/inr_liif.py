245f4c9e17d545b004a527bcda55388b17efa988
Miles Gray <mgray16@gmail.com>
2021-11-21 17:54:10 -0600

    Fix LIIF INR model in registry

--- models/inr_liif.py
+++ models/inr_liif.py
@@ -196,11 +196,11 @@ def make_liif_inr(encoder_spec, imnet_spec=None,
                  local_ensemble=True, feat_unfold=True, cell_decode=True,
                  fourier_features=64):
     args = {}
-    args["encoder_spec"] = encoder_spec
-    args["imnet_spec"] = imnet_spec
-    args["local_ensemble"]=local_ensemble
-    args["feat_unfold"] = feat_unfold
-    args["cell_decode"] = cell_decode
-    args["fourier_features"] = fourier_features
+    args.encoder_spec = encoder_spec
+    args.imnet_spec = imnet_spec
+    args.local_ensemble=local_ensemble
+    args.feat_unfold = feat_unfold
+    args.cell_decode = cell_decode
+    args.fourier_features = fourier_features
     
     return LIIF_INR(args)
