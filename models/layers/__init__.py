81b6cd3429c5c3226da06c8a78c0c88a3a337388
Miles Gray <mgray16@gmail.com>
2021-12-18 20:03:26 -0600

    Add activation funciton layers

--- models/layers/__init__.py
+++ models/layers/__init__.py
@@ -4,6 +4,8 @@ from .gate import *
 from .statistics import *
 from .pool import *
 
+from .activations import *
+
 from .hessian_attn import HessianAttention, MSHF, DiEnDec, DAC
 from .pixel_attn import PixelAttention
 from .channel_attn import ChannelAttention, MixPoolChannelAttention
@@ -14,7 +16,38 @@ from .balanced_attn import BalancedAttention
 from .cutout import MakeCutouts, WarpRandomPerspective
 
 import functools
+import torch.nn as nn
 import torch.nn.functional as F
+
+def create_activation(activation_type: str, *args, **kwargs) -> nn.Module:
+    if activation_type == 'leaky_relu':
+        return nn.LeakyReLU(*args, **kwargs)
+    elif activation_type == 'scaled_leaky_relu':
+        return ScaledLeakyReLU(*args, **kwargs)
+    elif activation_type == 'relu':
+        return nn.ReLU(*args, **kwargs)
+    elif activation_type == 'tanh':
+        return nn.Tanh(*args, **kwargs)
+    elif activation_type == 'none' or activation_type is None:
+        return nn.Identity(*args, **kwargs)
+    elif activation_type == 'centered_sigmoid':
+        return CenteredSigmoid(*args, **kwargs)
+    elif activation_type == 'sine':
+        return Sine(*args, **kwargs)
+    elif activation_type == 'unit_centered_sigmoid':
+        return UnitCenteredSigmoid(*args, **kwargs)
+    elif activation_type == 'unit_centered_tanh':
+        return UnitCenteredTanh(*args, **kwargs)
+    elif activation_type == 'sines_cosines':
+        return SinesCosines(*args, **kwargs)
+    elif activation_type == 'normalizer':
+        return Normalizer(*args, **kwargs)
+    elif activation_type == 'normal_clip':
+        return NormalClip(*args, **kwargs)
+    else:
+        raise NotImplementedError(f'Unknown activation type: {activation_type}')
+
+
 
 def reduce_loss(loss, reduction):
     """Reduce loss as specified.
---
+++ models/layers/activations.py
@@ -0,0 +1,71 @@
+import torch
+import torch.nn as nn
+
+from .registry import register
+
+
+
+@register("act_centered_sigmoid")
+class CenteredSigmoid(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        return x.sigmoid() * 2 - 1
+
+@register("act_unit_centered_sigmoid")
+class UnitCenteredSigmoid(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        return x.sigmoid() * 2
+
+@register("act_centered_tanh")
+class UnitCenteredTanh(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        return x.tanh() + 1
+
+@register("act_sine")
+class Sine(nn.Module):
+    def __init__(self, scale: float=1.0):
+        super(Sine, self).__init__()
+        self.scale = scale
+
+    def forward(self, x: Tensor) -> Tensor:
+        return x.sin()
+
+@register("act_normalizer")
+class Normalizer(nn.Module):
+    """
+    Just normalizes its input
+    """
+    def forward(self, x: Tensor) -> Tensor:
+        assert x.ndim == 2, f"Wrong shape: {x.shape}"
+
+        return (x - x.mean(dim=1, keepdim=True)) / x.std(dim=1, keepdim=True)
+
+@register("act_normal_clip")
+class NormalClip(nn.Module):
+    """
+    Clips input values into [-2, 2] region
+    """
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.clamp(x, -2, 2)
+
+@register("act_sines_cosines")
+class SinesCosines(nn.Module):
+    """
+    Sines-cosines activation function
+    It applies both sines and cosines and concatenates the results
+    """
+    def forward(self, x: Tensor) -> Tensor:
+        return torch.cat([x.sin(), x.cos()], dim=1)
+
+@register("act_scaled_leaky_relu")
+class ScaledLeakyReLU(nn.Module):
+    def __init__(self, negative_slope: float=0.2, scale: float=None):
+        super().__init__()
+
+        self.negative_slope = negative_slope
+        self.scale = math.sqrt(2 / (1 + negative_slope ** 2)) if scale is None else scale
+
+    def forward(self, input):
+        out = F.leaky_relu(input, negative_slope=self.negative_slope)
+
+        return out * self.scale
+     
