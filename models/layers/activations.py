c08a50401182c877ec209afe79d77eef24521d71
Miles Gray <mgray16@gmail.com>
2021-12-19 19:02:01 -0600

    Add more activatiomns

--- models/layers/activations.py
+++ models/layers/activations.py
@@ -5,6 +5,76 @@ from torch import Tensor
 from .registry import register
 
 
+@torch.jit.script
+def mish(x):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    See additional documentation for mish class.
+    '''
+    return x * torch.tanh(F.softplus(x))
+
+@register("act_mish")
+class Mish(nn.Module):
+    '''
+    Applies the mish function element-wise:
+    mish(x) = x * tanh(softplus(x)) = x * tanh(ln(1 + exp(x)))
+    Shape:
+        - Input: (N, *) where * means, any number of additional
+          dimensions
+        - Output: (N, *), same shape as the input
+    Examples:
+        >>> m = Mish()
+        >>> input = torch.randn(2)
+        >>> output = m(input)
+    '''
+    def __init__(self):
+        '''
+        Init method.
+        '''
+        super().__init__()
+
+    def forward(self, input):
+        '''
+        Forward pass of the function.
+        '''
+        return mish(input)
+
+@torch.jit.script
+def logcosh(x):
+    return torch.cosh(x + 1e-12).log()
+@register("act_logcosh")
+class LogCosh(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return logcosh(x)
+
+@torch.jit.script
+def xtanh(x):
+    return torch.tanh(x) * x
+@register("act_xtanh")
+class XTanh(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return xtanh(x)
+
+@torch.jit.script
+def xsigmoid(x):
+    y = 1 + torch.exp(-x)
+    y = torch.abs(y - x)
+    z = 2 * y / x
+    return z
+@register("act_xsigmoid")
+class XSigmoid(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return xsigmoid(x)
 
 @register("act_centered_sigmoid")
 class CenteredSigmoid(nn.Module):
@@ -24,7 +94,7 @@ class UnitCenteredTanh(nn.Module):
 @register("act_sine")
 class Sine(nn.Module):
     def __init__(self, scale: float=1.0):
-        super(Sine, self).__init__()
+        super().__init__()
         self.scale = scale
 
     def forward(self, x: Tensor) -> Tensor:
