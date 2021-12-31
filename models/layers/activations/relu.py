e983b34697a82b39e3402eb230409e26749b53a1
Miles Gray <mgray16@gmail.com>
2021-12-31 15:00:31 -0600

    Add missing components from IMDN

--- models/layers/activations/relu.py
+++ models/layers/activations/relu.py
@@ -40,3 +40,17 @@ class AdaptiveLeakyReLU(nn.Module):
         out = F.leaky_relu(x, negative_slope=self.negative_slope)
 
         return self.scale(out)
+
+@register("tlu")
+class TLU(nn.Module):
+    """ Thresholded Linear Unit """
+    def __init__(self, num_features):
+        super().__init__()
+        self.num_features = num_features
+        self.tau = nn.Parameter(torch.zeros(1, num_features, 1, 1))
+
+    def forward(self, x):
+        return torch.max(x, self.tau)
+
+    def extra_repr(self):
+        return 'num_features={}'.format(self.num_features)
