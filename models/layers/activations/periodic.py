986670917e475ecb40b5e7d30786818372b53560
Miles Gray <mgray16@gmail.com>
2022-01-11 20:28:51 -0600

    Move sine activation function to activation functions module, properly use create function to instantiate activation functions passed into MLP model

--- models/layers/activations/periodic.py
+++ models/layers/activations/periodic.py
@@ -8,15 +8,94 @@ from .methods import *
 
 from .registry import register
 
-@register("sine")
-class Sine(nn.Module):
-    def __init__(self, scale: float=1.0, learnable: bool=False):
+@register("sine_learnable")
+class SineLearnable(nn.Module):
+    def __init__(self, w0: float=1.0, 
+                 scale: float=1.0, 
+                 learnable: bool=False):
+        """Sine activation function with w0 scaling support along with
+            a second learnable scaling factor applied to the output.
+        Example:
+            >>> w = torch.tensor([3.14, 1.57])
+            >>> Sine(w0=1)(w)
+            torch.Tensor([0, 1])
+        :param w0: w0 in the activation step `act(x; w0) = s * sin(w0 * x)`.
+            defaults to 1.0
+        :type w0: float, optional
+        :param scale: s in the activation step `act(x; w0; s) = s * sin(w0 * x)`.
+            defaults to 1.0
+        :type scale: float, optional
+        :param learnable: enables updates to `s` value in activation step  
+            `act(x; w0; s) = s * sin(w0 * x)`.
+        :type learnable: float, optional
+        """
         super().__init__()
-        self.scale = Scale(scale) if learnable else scale
+        self.w0 = w0
+        self.scale = Scale(scale) if learnable else nn.Identity()
 
     def forward(self, x: Tensor) -> Tensor:
-        return x.sin()
+        return self.scale(torch.sin(self.w0 * x))
     
+    @staticmethod
+    def init(m):
+        with torch.no_grad():
+            if hasattr(m, 'weight'):
+                print('sine_init for Siren...')
+                num_input = m.weight.size(-1)
+                # See supplement Sec. 1.5 for discussion of factor 30
+                m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+                
+    @staticmethod
+    def first_layer_init(m):
+        with torch.no_grad():
+            if hasattr(m, 'weight'):
+                print('first_layer_sine_init for Siren...')
+                num_input = m.weight.size(-1)
+                # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
+                m.weight.uniform_(-1 / num_input, 1 / num_input)    
+
+@register('sine')
+class Sine(nn.Module):
+    def __init__(self, w0: float = 1.0):
+        """Sine activation function with w0 scaling support.
+        Example:
+            >>> w = torch.tensor([3.14, 1.57])
+            >>> Sine(w0=1)(w)
+            torch.Tensor([0, 1])
+        :param w0: w0 in the activation step `act(x; w0) = sin(w0 * x)`.
+            defaults to 1.0
+        :type w0: float, optional
+        """
+        super().__init__()
+        self.w0 = w0
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        self._check_input(x)
+        return torch.sin(self.w0 * x)
+
+    @staticmethod
+    def _check_input(x):
+        if not isinstance(x, torch.Tensor):
+            raise TypeError('input to forward() must be torch.Tensor')
+        
+    @staticmethod
+    def init(m):
+        with torch.no_grad():
+            if hasattr(m, 'weight'):
+                print('sine_init for Siren...')
+                num_input = m.weight.size(-1)
+                # See supplement Sec. 1.5 for discussion of factor 30
+                m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+                
+    @staticmethod
+    def first_layer_init(m):
+        with torch.no_grad():
+            if hasattr(m, 'weight'):
+                print('first_layer_sine_init for Siren...')
+                num_input = m.weight.size(-1)
+                # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
+                m.weight.uniform_(-1 / num_input, 1 / num_input)
+                    
 
 @register("sines_cosines")
 class SinesCosines(nn.Module):
@@ -26,4 +105,3 @@ class SinesCosines(nn.Module):
     """
     def forward(self, x: Tensor) -> Tensor:
         return torch.cat([x.sin(), x.cos()], dim=1)
-
--- models/mlp.py
+++ models/mlp.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 
 from models import register
 from .layers import LinearResidual
+from .layers import create as create_layer
+from .layers.activations import create as create_act
 
 @register('mlp')
 class MLP(nn.Module):
@@ -19,12 +21,12 @@ class MLP(nn.Module):
         self.norm = norm
         if act is None:
             self.act = None
-        elif act.lower() == 'relu':
-            self.act = nn.ReLU() 
-        elif act.lower() == 'gelu':
-            self.act = nn.GELU()
-        elif act.lower() == 'sine':
-            self.act = Sine()
+        elif isinstance(act, str):
+            self.act = create_act(act.lower())
+        elif isinstance(act, nn.Module):
+            self.act = act
+            act = str(type(act)).split("'")[-2]
+            act = act.split(".")[-1] if "." in act else act
         else:
             assert False, f'activation {act} is not supported'
             
@@ -53,8 +55,8 @@ class MLP(nn.Module):
         layers.append(nn.Linear(lastv, out_dim))
         self.layers = nn.Sequential(*layers)
         if act is not None and act.lower() == 'sine':
-            self.layers.apply(sine_init)
-            self.layers[0].apply(first_layer_sine_init)
+            self.layers.apply(self.act.init)
+            self.layers[0].apply(self.act.first_layer_init)
 
     def forward(self, x):
         shape = x.shape[:-1]
@@ -71,7 +73,7 @@ class SIREN(nn.Module):
             layers.append(nn.Linear(lastv, hidden, bias=has_bias))
             if has_bn:
                 layers.append(nn.BatchNorm1d(hidden))
-            layers.append(Sine(w0=w0))            
+            layers.append(create_act('sine', {"w0": w0}))
             lastv = hidden
         layers.append(nn.Linear(lastv, out_dim))
         self.layers = nn.Sequential(*layers)
@@ -81,46 +83,6 @@ class SIREN(nn.Module):
         x = self.layers(x.view(-1, x.shape[-1]))
         return x.view(*shape, -1)
 
-@register('sine')
-class Sine(nn.Module):
-    def __init__(self, w0: float = 1.0):
-        """Sine activation function with w0 scaling support.
-        Example:
-            >>> w = torch.tensor([3.14, 1.57])
-            >>> Sine(w0=1)(w)
-            torch.Tensor([0, 1])
-        :param w0: w0 in the activation step `act(x; w0) = sin(w0 * x)`.
-            defaults to 1.0
-        :type w0: float, optional
-        """
-        super().__init__()
-        self.w0 = w0
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        self._check_input(x)
-        return torch.sin(self.w0 * x)
-
-    @staticmethod
-    def _check_input(x):
-        if not isinstance(x, torch.Tensor):
-            raise TypeError('input to forward() must be torch.Tensor')
-
-def sine_init(m):
-    with torch.no_grad():
-        if hasattr(m, 'weight'):
-            print('sine_init for Siren...')
-            num_input = m.weight.size(-1)
-            # See supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
-
-def first_layer_sine_init(m):
-    with torch.no_grad():
-        if hasattr(m, 'weight'):
-            print('first_layer_sine_init for Siren...')
-            num_input = m.weight.size(-1)
-            # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
-            m.weight.uniform_(-1 / num_input, 1 / num_input)
-            
 def init_weights(m):
     # if hasattr(modules, 'weight'):
     if isinstance(m, nn.Linear):
