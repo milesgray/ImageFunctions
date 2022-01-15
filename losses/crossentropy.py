Add frequency loss file with FS Loss, add to losses init and fix weighted_loss function; add gradient loss; update config files with new loss system

losses/crossentropy.py
@@ -23,7 +23,7 @@ def reduce_loss(loss, reduction):
     Return:
         Tensor: Reduced loss tensor.
     """
-    reduction_enum = F._Reduction.get_enum(reduction)
+    reduction_enum = torch.nn.functional._Reduction.get_enum(reduction)
     # none: 0, elementwise_mean:1, sum: 2
     if reduction_enum == 0:
         return loss