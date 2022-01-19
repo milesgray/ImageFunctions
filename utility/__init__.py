fb377fa215eb5cf9a223e7f1fb9932d69f6ddc36
Miles Gray <mgray16@gmail.com>
2022-01-18 19:24:49 -0600

    Add persistence code

--- utility/__init__.py
+++ utility/__init__.py
@@ -66,3 +66,18 @@ def dict_flatten(d, prefix='', join_char='/'):
         else:
             out[cur_k] = v
     return out
+
+class EasyDict(dict):
+    """Convenience class that behaves like a dict but allows access with the attribute syntax."""
+
+    def __getattr__(self, name: str) -> Any:
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        self[name] = value
+
+    def __delattr__(self, name: str) -> None:
+        del self[name]
