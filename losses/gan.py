722ac22624a03be9d078180bd0d97d8806c3fee0
Miles Gray <mgray16@gmail.com>
2022-01-20 06:00:23 -0600

    Minor

--- losses/gan.py
+++ losses/gan.py
@@ -7,7 +7,7 @@ from argparse import Namespace
 from .registry import register
 
 
-@register("dcgan_disc_loss")
+@register("dcgan_disc")
 class DCGANDiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -18,7 +18,7 @@ class DCGANDiscriminatorLoss(nn.Module):
         dis_loss = -torch.mean(nn.LogSigmoid()(dis_out_real) + nn.LogSigmoid()(ones - dis_out_fake))
         return dis_loss
 
-@register("dcgan_gen_loss")
+@register("dcgan_gen")
 class DCGANGeneratorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -26,7 +26,7 @@ class DCGANGeneratorLoss(nn.Module):
     def forward(self, dis_out_fake):
         return -torch.mean(nn.LogSigmoid()(gen_out_fake))
 
-@register("lsgan_disc_loss")
+@register("lsgan_disc")
 class LSGANDiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -35,7 +35,7 @@ class LSGANDiscriminatorLoss(nn.Module):
         dis_loss = 0.5*(dis_out_real - torch.ones_like(dis_out_real))**2 + 0.5*(dis_out_fake)**2
         return dis_loss.mean()
 
-@register("lsgan_gen_loss")
+@register("lsgan_gen")
 class LSGANGeneratorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -44,7 +44,7 @@ class LSGANGeneratorLoss(nn.Module):
         dis_loss = 0.5*(dis_out_real - torch.ones_like(dis_out_real))**2 + 0.5*(dis_out_fake)**2
         return dis_loss.mean()
 
-@register("hinge_disc_loss")
+@register("gan_hinge_disc")
 class HingeDiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -52,7 +52,7 @@ class HingeDiscriminatorLoss(nn.Module):
     def forward(self, dis_out_real, dis_out_fake):
         return torch.mean(F.relu(1. - dis_out_real)) + torch.mean(F.relu(1. + dis_out_fake))
 
-@register("hinge_gen_loss")
+@register("gan_hinge_gen")
 class HingeDiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -60,7 +60,7 @@ class HingeDiscriminatorLoss(nn.Module):
     def forward(self, gen_out_fake):
         return -torch.mean(gen_out_fake)
 
-@register("wgan_disc_loss")
+@register("gan_wgan_disc")
 class WGANDiscriminatorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -68,7 +68,7 @@ class WGANDiscriminatorLoss(nn.Module):
     def forward(self, dis_out_real, dis_out_fake):
         return torch.mean(dis_out_fake - dis_out_real)
 
-@register("wgan_gen_loss")
+@register("gan_wgan_gen")
 class WGANGeneratorLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -77,7 +77,7 @@ class WGANGeneratorLoss(nn.Module):
         return -torch.mean(gen_out_fake)
 
 
- 
+@register("gan_grad")
 class GradLoss(nn.Module):
     def __init__(self, style):
         super().__init__()
@@ -90,6 +90,7 @@ class GradLoss(nn.Module):
             loss = grad_loss.compute_r1_penalty(disc_model, real)
         return loss
 
+@register("gan_nonsaturating")
 class NonSaturatingLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -97,7 +98,7 @@ class NonSaturatingLoss(nn.Module):
     def forward(self, fake):
         return (-F.softsign(fake)).mean()
 
-
+@register("gan_hinge")
 class HingeLoss(nn.Module):
     def __init__(self):
         super().__init__()
@@ -106,3 +107,86 @@ class HingeLoss(nn.Module):
         real = F.normalize(real, dim=-1, p=2)
         fake = F.normalize(fake, dim=-1, p=2)
         return (F.relu(1 + real) + F.relu(1 - fake)).mean()
+@register("gan_adversarial")
+class AdversarialLoss(nn.Module):
+    def __init__(self, gan_type='vanilla', real_label_val=1.0, fake_label_val=0.0):
+        """
+        Args:
+            gan_type (str): Support 'vanilla', 'lsgan', 'wgan', 'hinge'.
+            real_label_val (float): The value for real label. Default: 1.0.
+            fake_label_val (float): The value for fake label. Default: 0.0.
+        """
+
+        super(AdversarialLoss, self).__init__()
+
+        self.gan_type = gan_type
+        self.real_label_val = real_label_val
+        self.fake_label_val = fake_label_val
+
+        if self.gan_type == 'vanilla':
+            self.loss = nn.BCEWithLogitsLoss()
+        elif self.gan_type == 'lsgan':
+            self.loss = nn.MSELoss()
+        elif self.gan_type == 'wgan':
+            self.loss = self._wgan_loss
+        elif self.gan_type == 'wgan_softplus':
+            self.loss = self._wgan_softplus_loss
+        elif self.gan_type == 'hinge':
+            self.loss = nn.ReLU()
+        else:
+            raise NotImplementedError('GAN type %s is not implemented.' % self.gan_type)
+
+    def _wgan_loss(self, input, target):
+        """
+        Args:
+            input (Tensor): Input tensor.
+            target (bool): Target label.
+        """
+        return -input.mean() if target else input.mean()
+
+    def _wgan_softplus_loss(self, input, target):
+        """wgan loss with soft plus. softplus is a smooth approximation to the ReLU function.
+        In StyleGAN2, it is called:
+            Logistic loss for discriminator;
+            Non-saturating loss for generator.
+        Args:
+            input (Tensor): Input tensor.
+            target (bool): Target label.
+        """
+        return F.softplus(-input).mean() if target else F.softplus(input).mean()
+
+    def get_target_label(self, input, target_is_real):
+        """Get target label.
+        Args:
+            input (Tensor): Input tensor.
+            target_is_real (bool): Whether the target is real or fake.
+        Returns:
+            (bool | Tensor): Target tensor. Return bool for wgan, otherwise, return Tensor.
+        """
+        if self.gan_type in ['wgan', 'wgan_softplus']:
+            return target_is_real
+        target_val = (self.real_label_val if target_is_real else self.fake_label_val)
+        return input.new_ones(input.size()) * target_val
+
+    def forward(self, input, target_is_real, is_disc=False):
+        """
+        Args:
+            input (Tensor): The input for the loss module, i.e., the network
+                prediction.
+            target_is_real (bool): Whether the targe is real or fake.
+            is_disc (bool): Whether the loss for discriminators or not.
+                Default: False.
+        Returns:
+            Tensor: GAN loss value.
+        """
+        target_label = self.get_target_label(input, target_is_real)
+        if self.gan_type == 'hinge':
+            if is_disc:
+                input = -input if target_is_real else input
+                loss = self.loss(1 + input).mean()
+            else:
+                loss = -input.mean()
+        else:  # other gan types
+            loss = self.loss(input, target_label)
+
+        return loss
