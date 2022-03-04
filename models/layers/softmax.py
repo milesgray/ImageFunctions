5c4f75a68eb06b74a8902ab02a29edc8474c0910
Miles Gray <mgray16@gmail.com>
2022-03-03 23:52:40 -0600

    Add gumbel softmax option

--- models/layers/softmax.py
+++ models/layers/softmax.py
@@ -9,7 +9,7 @@ class SpatialSoftmax2d(nn.Module):
 
     def forward(self, x):
         x = self.softmax(x)
-        return x * self.temp
+        return x * self.temp.to(x.device)
 
 class ChannelSoftmax2d(nn.Module):
     def __init__(self, temp: float=1.0, requires_grad: bool=True):
@@ -19,4 +19,18 @@ class ChannelSoftmax2d(nn.Module):
 
     def forward(self, x):
         x = self.softmax(x)
-        return x * self.temp
+        return x * self.temp.to(x.device)
+    
+    
+class ChannelGumbelMax2d(nn.Module):
+    def __init__(self, tau: float=1.0, hard: bool=False, dim: int=-1, requires_grad: bool=True):
+        super().__init__()
+        self.tau = tau
+        self.hard = hard
+        self.dim = dim
+        self.temp = nn.Parameter(torch.FloatTensor([temp]), requires_grad=requires_grad)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x = F.gumbel_softmax(x, tau=self.tau, hard=self.hard, dim=self.dim)
+        return x * self.temp.to(x.device)
